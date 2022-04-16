27b24d485a52fb0344bea2bf0c8ab583188075a9
Sanjay Byrannanavar <sbsa597@gmail.com>
2022-04-16 18:21:12 +0530

    Update True wireless.bat

--- True wireless.bat
+++ True wireless.bat
@@ -1,8 +1,10 @@
 @echo off 
 echo deleting adb cache 
+
 adb disconnect
-adb kill-server
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 cls
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo made by sanjaybyrannanavar
 echo.
 echo No need to Connect any USB for this process, 
@@ -11,41 +13,56 @@ echo But mobile wifi and computer wifi should be connected to same Hotspot netwo
 echo.
 echo Note that u can't use your mobile hotspot and connect computer wifi to it, instead u must connect both devices wifi to another Hotspot network.
 echo.
-pause
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+TIMEOUT /T 60
 cls
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo.
 echo Find IP adress in : About phone---status information---IPadress section
+echo.
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
 set /p IPADD= IP Address from about phone :
 cls
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo.
 echo Find Last five digit of IP port and Pairing code in : Devoloper's option---wireless debugging---Pairing code and port
 echo.
 echo For Example : If ip Port is %IPADD%:50550 then 50550 is the last five digit of IP port address
 echo.
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
 set /p IPPORT= Enter Last Five Digit of IP port :
+echo.
 adb pair %IPADD%:%IPPORT% 
+echo.
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
 set /p answer2= Are U able to connect successfully (Y/N)?:
 if %answer2%==n (echo.
 echo then restart the BAT file and Follow correct procedure.
 echo.
-pause
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo.
+TIMEOUT /T  10 
 exit)
+
 if %answer2%==y (cls
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
 echo connecting via TCPIP 5555......
-echo running adb connect %IPADD%.....
+echo running adb connect %IPADD%
 echo.
 adb connect %IPADD% )
-
 echo.
+echo ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
+echo Scrcpy will start in 10 sec, So U are
+TIMEOUT /T 10
 echo.
-set /p answer3= Do u want to start scrcpy (Y/N)?:
-if %answer3%==y (cls
 scrcpy 
 pause
-exit)
+exit
 echo.
 if %answer3%==n (cls 
 exit)
